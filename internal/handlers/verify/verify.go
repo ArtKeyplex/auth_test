0cc728fc72cf8013b8058039d8cc2621dbcb1356
Daniil Lifatov <2004daniil9015@mail.ru>
2025-07-24 21:06:18 +0300

    openapi/swagger

--- internal/handlers/verify/verify.go
+++ internal/handlers/verify/verify.go
@@ -15,14 +15,13 @@ func (h *VerifyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	authHeader := r.Header.Get("Authorization")
 	if authHeader == "" || !strings.HasPrefix(authHeader, "Bearer ") {
 		w.WriteHeader(http.StatusUnauthorized)
-		w.Write([]byte("заголовок пустой или неверный"))
 		return
 	}
 	token := strings.TrimPrefix(authHeader, "Bearer ")
 	newToken, err := h.UserService.RefreshToken(token)
 	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		w.Write([]byte("ошибка в методе refresh token\n" + err.Error()))
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("error: " + err.Error()))
 		return
 	}
 	w.WriteHeader(http.StatusOK)
